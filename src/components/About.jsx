500ec72291852f525383f327059a38ad0bb7bee9
Pavan Yadav <pavan.yadav.sde@gmail.com>
2026-04-15 00:55:37 +0530

    Update skill percentages and About section layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,72 +1,72 @@
 import React from 'react';
 
 function About() {
-  return (
-    <>
-        <section id="about" className="mt-4 mb-4">
-            <h4 className="display-4">A little about me,</h4>
-            <div className="row">
-                <div className="col-lg-6 wrap text-left">
-                    <div className="about-desc">
-                        <h1 className="bold-text">About</h1>
-                        <div className="pt-5">
-                            <h2 className="mb-5">Hello!</h2>
-                            <p>My name is Pavan. I am a pre-final year undergraduate at Government Engineering College,
-                                Dahod, and IIT Madras (BS program in Data Science and Applications). I am an avid learner with
-                                a keen interest in Computer Programming Languages, Data Science (AI/ML), and Software
-                                Development.
-                            </p>
+    return (
+        <>
+            <section id="about" className="mt-4 mb-4">
+                <h4 className="display-4">A little about me,</h4>
+                <div className="row">
+                    <div className="col-lg-6 wrap text-left">
+                        <div className="about-desc">
+                            <h1 className="bold-text">About</h1>
+                            <div className="pt-5">
+                                <h2 className="mb-5">Hello!</h2>
+                                <p>My name is Pavan. I am a pre-final year undergraduate at Government Engineering College,
+                                    Dahod, and IIT Madras (BS program in Data Science and Applications). I am an avid learner with
+                                    a keen interest in Computer Programming Languages, Data Science (AI/ML), and Software
+                                    Development.
+                                </p>
 
 
-                            <ul className="social-links list-unstyled mt-4">
-                                <li><a target="_blank" href="mailto:pavan.yadav.sde@gmail.com">
+                                <ul className="social-links list-unstyled mt-4">
+                                    <li><a target="_blank" href="mailto:pavan.yadav.sde@gmail.com">
                                         <ion-icon name="mail-outline"></ion-icon>
                                     </a></li>
-                                <li><a target="_blank" href="https://github.com/PAVAN2005-LAB">
+                                    <li><a target="_blank" href="https://github.com/PAVAN2005-LAB">
                                         <ion-icon name="logo-github"></ion-icon>
                                     </a></li>
-                                <li><a target="_blank"
+                                    <li><a target="_blank"
                                         href="https://www.linkedin.com/in/gecdhd-comp-pavan-kumar-yadav/">
                                         <ion-icon name="logo-linkedin"></ion-icon>
                                     </a></li>
 
-                            </ul>
+                                </ul>
 
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="col-lg-6 wrap align-content-center">
-                    <div className="row mt-5 flex-column">
-                        <div className="col-md-8">
-                            <h2 className="mb-4">My Skills</h2>
-                        </div>
-                        <div className="col-md-6 mt-3">
-                            <div className="progress-wrap">
-                                <h5>Full Stack Development (MERN)</h5>
-                                <div className="progress progress-style">
-                                    <div className="progress-bar color-1" role="progressbar" aria-valuenow="75"
-                                        aria-valuemin="0" aria-valuemax="100" style={{'width': '75%'}}>
+                    <div className="col-lg-6 wrap align-content-center">
+                        <div className="row mt-5 flex-column">
+                            <div className="col-md-8">
+                                <h2 className="mb-4">My Skills</h2>
+                            </div>
+                            <div className="col-md-6 mt-3">
+                                <div className="progress-wrap">
+                                    <h5>Full Stack Development (MERN)</h5>
+                                    <div className="progress progress-style">
+                                        <div className="progress-bar color-1" role="progressbar" aria-valuenow="65"
+                                            aria-valuemin="0" aria-valuemax="100" style={{ 'width': '65%' }}>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-6 mt-3">
+                                <div className="progress-wrap">
+                                    <h5>Data Science</h5>
+                                    <div className="progress progress-style">
+                                        <div className="progress-bar color-1" role="progressbar" aria-valuenow="50"
+                                            aria-valuemin="0" aria-valuemax="100" style={{ 'width': '50%' }}>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-md-6 mt-3">
-                            <div className="progress-wrap">
-                                <h5>Data Science</h5>
-                                <div className="progress progress-style">
-                                    <div className="progress-bar color-1" role="progressbar" aria-valuenow="30"
-                                        aria-valuemin="0" aria-valuemax="100" style={{'width': '30%'}}>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
 
-    </>
-  );
+        </>
+    );
 }
 
 export default About;
--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -1,46 +1,54 @@
 import React from 'react';
 
 function Achievements() {
-  return (
-    <>
-    <section id="achievements">
-        <h1 className="display-4 mt-5">Achievements</h1>
-        <h1 className="bold-text bg-text">Honours</h1>
-        <div className="row d-flex">
-            <div className="col-md-10">
-                <div className="mt-3">
+    return (
+        <>
+            <section id="achievements">
+                <h1 className="display-4 mt-5">Achievements</h1>
+                <h1 className="bold-text bg-text">Honours</h1>
+                <div className="row d-flex">
+                    <div className="col-md-10">
+                        <div className="mt-3">
 
-                    <div className="py-4">
-                        <div className="desc">
-                            <h4>Problem Solving</h4>
-                        </div>
-                        <div>
-                            <ul>
-                                <li>Solved 200+ coding problems over various platforms.</li>
-                                <li><a target="_blank" href="https://leetcode.com/u/Pavanslab_2005/">LeetCode Profile</a></li>
-                                <li>Max. Pupil, 1189 at <a target="_blank" href="https://codeforces.com/profile/pavan.yadav.27">Codeforces</a></li>
-                                <li><a target="_blank" href="https://www.geeksforgeeks.org/profile/pavanyaadwp">GeeksForGeeks Profile</a></li>
-                            </ul>
-                        </div>
-                    </div>
+                            <div className="py-4">
+                                <div className="desc">
+                                    <h4>Problem Solving</h4>
+                                </div>
+                                <div>
+                                    <li>Solved 200+ coding problems over various platforms. </li>
+                                    <li><a target="_blank" href="https://leetcode.com/u/Pavanslab_2005/">LeetCode Profile</a>
+                                    </li>
+                                    <li> max. Pupil, 1189 at <a target="_blank"
+                                        href="https://codeforces.com/profile/pavan.yadav.27">Codeforces</a></li>
 
-                    <div className="py-4">
-                        <div className="desc">
-                            <h4>TCS CodeVita</h4>
-                        </div>
-                        <div>
-                            <ul>
-                                <li>Secured global ranks 4918 and 740 in TCS CodeVita Session 12 and Session 13 respectively.</li>
-                            </ul>
+                                    <li><a target="_blank"
+                                        href="https://www.geeksforgeeks.org/profile/pavanyaadwp">GeeksForGeeks
+                                        Profile</a></li>
+
+
+
+                                </div>
+                            </div>
+
+
+
+
+                            <div className="py-4">
+                                <div className="desc">
+                                    <h4>TCScodevita</h4>
+                                </div>
+                                <div>
+                                    <li>secured global rank 4918 and 740 in TCScodevita session 12 and session 13 respectively.
+                                    </li>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </section>
+            </section>
 
-    </>
-  );
+        </>
+    );
 }
 
 export default Achievements;
